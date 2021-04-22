030221b4c21fd1fe51472d1f785da16d19da734b
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-22 10:44:00 +0100

    first test tests an asnyc function

--- spec/userCanViewPeeps.js
+++ spec/userCanViewPeeps.js
@@ -1,9 +1,12 @@
 describe('User can view a list of peeps', () => {
-  it('shows a list of peeps', () => {
+  it('shows a list of peeps', (done) => {
     let controller = new PeepListController();
-    controller.display();
-    let container = document.querySelector("#main");
-    expect(container.innerHTML).toEqual("<ul><li>Hello, world!</li></ul>")
+    controller.display().then(() => {
+      let container = document.querySelector("#main");
+      expect(container.innerHTML).toEqual("<ul><li>yo wadupppp</li></ul>")
+      done();
+    });
   });
 });
 
+// Using done to specify when async testing is finished.
--- src/PeepListController.js
+++ src/PeepListController.js
@@ -1,6 +1,14 @@
 class PeepListController {
   display() {
     let container = document.querySelector("#main");
-    container.innerHTML = "<ul><li>Hello, world!</li></ul>";
+    return fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
+      .then(response => response.json())
+      .then(data => {
+        let peep = data[0];
+        container.innerHTML = `<ul><li>${peep.body}</li></ul>`;
+      });
+    
   }
 }
+
+// Each fetch returns a promise, so does .then
