7127fe59f2d8f858f227fa552b8b05f1dfd45323
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-22 10:22:20 +0100

    first test

---
+++ spec/userCanViewPeeps.js
@@ -0,0 +1,9 @@
+describe('User can view a list of peeps', () => {
+  it('shows a list of peeps', () => {
+    let controller = new PeepListController();
+    controller.display();
+    let container = document.querySelector("#main");
+    expect(container.innerHTML).toEqual("<ul><li>Hello, world!</li></ul>")
+  });
+});
+
